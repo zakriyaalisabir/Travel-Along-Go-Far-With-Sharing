810ea4166bcd26eeb6b14891e3dbf898b1c7736d
zakriyaalisabir <zakriyaalisabir10@gmail.com>
2018-10-20 23:51:07 +0500

    updated gradle file

--- app/src/main/java/com/zakriyaalisabir/travelalonggofarwithsharing/Login.java
+++ app/src/main/java/com/zakriyaalisabir/travelalonggofarwithsharing/Login.java
@@ -1,13 +1,41 @@
 package com.zakriyaalisabir.travelalonggofarwithsharing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnL,btnR;
+    private TextView tvFP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnL=(Button)findViewById(R.id.btnLogin);
+        btnR=(Button)findViewById(R.id.btnRegister);
+
+        tvFP=(TextView)findViewById(R.id.tvForgetPassword);
+
+        btnL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),FirstTimeLoginForMobileNumberVerification.class));
+                finish();
+            }
+        });
+
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/zakriyaalisabir/travelalonggofarwithsharing/SwipesAfterSplash.java
+++ app/src/main/java/com/zakriyaalisabir/travelalonggofarwithsharing/SwipesAfterSplash.java
@@ -1,6 +1,10 @@
 package com.zakriyaalisabir.travelalonggofarwithsharing;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +12,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SwipesAfterSplash extends AppCompatActivity {
 
@@ -37,6 +42,24 @@ public class SwipesAfterSplash extends AppCompatActivity {
                 finish();
             }
         });
+
+        int permission= ContextCompat.checkSelfPermission(SwipesAfterSplash.this,Manifest.permission.CAMERA);
+        int permission1= ContextCompat.checkSelfPermission(SwipesAfterSplash.this,Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permission2= ContextCompat.checkSelfPermission(SwipesAfterSplash.this,Manifest.permission.ACCESS_FINE_LOCATION);
+        int permission3= ContextCompat.checkSelfPermission(SwipesAfterSplash.this,Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permission4= ContextCompat.checkSelfPermission(SwipesAfterSplash.this,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permission!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        if(permission1!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        if(permission2!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        if(permission3!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+        if(permission4!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 4);
+
     }
 
     @Override
@@ -70,8 +93,6 @@ public class SwipesAfterSplash extends AppCompatActivity {
                         if(n<imagesForSwiping.length){
                             imv.setImageResource(imagesForSwiping[n]);
                         }
-
-
                     }
                     return true;
                 }
@@ -82,4 +103,99 @@ public class SwipesAfterSplash extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission granted to use your CAMERA", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission denied to use your CAMERA", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission granted to use course LOCATION service", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission denied to use course LOCATION service", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 2: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission granted to use fine LOCATION service", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission denied to use fine LOCATION service", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 3: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission granted to read your External storage", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 4: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission granted to write on your External storage", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SwipesAfterSplash.this, "Permission denied to write on your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
